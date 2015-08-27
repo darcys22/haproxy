5c5f7446d40482145e0d1d16b4eeb5adade16b76
Sean Darcy <darcys22@gmail.com>
2015-08-27 20:34:31 +1000

    slowly

--- boot.sh
+++ boot.sh
@@ -21,4 +21,4 @@ echo "[haproxy] confd is now monitoring etcd for changes..."
 
 # Start the Nginx service using the generated config
 echo "[haproxy] starting haproxy..."
-exec haproxy -f /usr/local/etc/haproxy/haproxy.cfg -p /var/run/haproxy.pid -d -D -sf $(cat /var/run/haproxy.pid)
+exec haproxy -f /etc/haproxy/haproxy.cfg -d -D 
